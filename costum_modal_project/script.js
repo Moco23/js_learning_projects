63bd621db56d0cb5a6dbee77c653db83754fddbf
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-06-08 20:33:33 +0200

    code optimizadion

--- costum_modal_project/script.js
+++ costum_modal_project/script.js
@@ -5,19 +5,22 @@ const overlay = document.querySelector(".overlay");
 const btn_close_moda = document.querySelector(".close-modal");
 const btns_open_modal = document.querySelectorAll(".show-modal");
 
-// create for lop for open modal buttons click fuction
-for (let i = 0; i < btns_open_modal.length; i++) {
-  btns_open_modal[i].addEventListener("click", function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
-}
+// open modal fuction
+const open_modal_function = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
 // close fuction
 const close_modal_function = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
+
+// create for lop for open modal buttons click fuction
+for (let i = 0; i < btns_open_modal.length; i++) {
+  btns_open_modal[i].addEventListener("click", open_modal_function);
+}
 
 // make a closing modal button click function
 btn_close_moda.addEventListener("click", close_modal_function);
