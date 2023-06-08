656f7cc1fb6249f061ee3d697bb3d97cbaf7ab7f
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-06-08 20:06:05 +0200

    set open fuction on click modal will be open

--- costum_modal_project/script.js
+++ costum_modal_project/script.js
@@ -5,9 +5,10 @@ const overlay = document.querySelector(".overlay");
 const btn_close_moda = document.querySelector(".close-modal");
 const btns_open_modal = document.querySelectorAll(".show-modal");
 
-// create for lop for open modal buttons
+// create for lop for open modal buttons click fuction
 for (let i = 0; i < btns_open_modal.length; i++) {
   btns_open_modal[i].addEventListener("click", function () {
-    console.log("open modal test");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
   });
 }
