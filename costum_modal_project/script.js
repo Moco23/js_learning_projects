d0856ed613852345f6b116118ba46a88579e692d
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-06-08 19:43:15 +0200

    set variables for get elements from html file

---
+++ costum_modal_project/script.js
@@ -0,0 +1,6 @@
+"use strict";
+// set variables for getting elements from html
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btn_close_moda = document.querySelector(".close-modal");
+const btns_open_modal = document.querySelectorAll(".show-modal");
