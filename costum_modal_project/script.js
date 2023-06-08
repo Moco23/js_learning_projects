2f1250fc7203d7b93061a58acde0a79e08cb9a75
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-06-08 19:53:31 +0200

    create for loop add event lisener click fuction on open modla btns

--- costum_modal_project/script.js
+++ costum_modal_project/script.js
@@ -1,6 +1,13 @@
 "use strict";
-// set variables for getting elements from html
+// set variables for geting elements from html
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btn_close_moda = document.querySelector(".close-modal");
 const btns_open_modal = document.querySelectorAll(".show-modal");
+
+// create for lop for open modal buttons
+for (let i = 0; i < btns_open_modal.length; i++) {
+  btns_open_modal[i].addEventListener("click", function () {
+    console.log("open modal test");
+  });
+}
