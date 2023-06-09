ec0db5ea6373ddadddac51dbe8d1852f469e8676
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-06-09 15:25:21 +0200

    rolling dice functionality with random number function

--- small_game/script.js
+++ small_game/script.js
@@ -3,8 +3,16 @@
 const score0EL = document.querySelector("#score--0");
 const score1EL = document.querySelector("#score--1");
 const diceEl = document.querySelector(".dice");
+const btn_new = document.querySelector(".btn--new");
+const btn_roll = document.querySelector(".btn--roll");
+const btn_hold = document.querySelector(".btn--hold");
 
 // startng conditions
 score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEl.classList.add("hidden");
+
+// Rolling ice functionality
+btn_roll.addEventListener("click", function () {
+  const dice = Math.tan(Math.random() * 6);
+});
