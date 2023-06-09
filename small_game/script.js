37a7f0e464c5f623fb79d918e6137f7f4d4f53ff
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-06-09 15:01:41 +0200

    add a start condition and hidden class on el

--- small_game/script.js
+++ small_game/script.js
@@ -2,3 +2,9 @@
 // selecting elements
 const score0EL = document.querySelector("#score--0");
 const score1EL = document.querySelector("#score--1");
+const diceEl = document.querySelector(".dice");
+
+// startng conditions
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEl.classList.add("hidden");
