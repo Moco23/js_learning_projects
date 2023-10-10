54b5b1d1ded5e16cb83e044b12d9aa44640f302d
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-10-10 16:13:14 +0200

    implement leaflet libery and display map on the screen

--- map_application/script.js
+++ map_application/script.js
@@ -20,6 +20,20 @@ if (navigator.geolocation)
       console.log(
         `https://www.google.com/maps/@${latitude},${longitude},1576m/data=!3m1!1e3?entry=ttu`
       );
+
+      const cords = [latitude, longitude];
+
+      const map = L.map("map").setView(cords, 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(cords)
+        .addTo(map)
+        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+        .openPopup();
     },
     function () {
       alert(`Could not get your position`);
