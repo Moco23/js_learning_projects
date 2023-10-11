af28b4a64a9346ce7678ff2935517257a8b100b2
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-10-11 11:57:50 +0200

    refactoring code

--- api_ajax/script.js
+++ api_ajax/script.js
@@ -5,34 +5,74 @@ const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
 // xmlhttp request
-const getCountryData = function (country) {
-  const xmlRequest = new XMLHttpRequest();
-  xmlRequest.open("GET", `https://restcountries.com/v2/name/${country}`);
-  xmlRequest.send();
-  xmlRequest.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+// const getCountryData = function (country) {
+//   const xmlRequest = new XMLHttpRequest();
+//   xmlRequest.open("GET", `https://restcountries.com/v2/name/${country}`);
+//   xmlRequest.send();
+//   xmlRequest.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    const html = `
-    <article class="country">
-      <img class="country__img" src="${data.flag}" />
-        <div class="country__data">
-          <h3 class="country__name">${data.name}</h3>
-          <h4 class="country__region">${data.region}</h4>
-          <p class="country__row"><span>👫</span>${(
-            +data.population / 1000000
-          ).toFixed(1)} people</p>
-          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-        </div>
-  </article>
-    `;
+//     const html = `
+//     <article class="country">
+//       <img class="country__img" src="${data.flag}" />
+//         <div class="country__data">
+//           <h3 class="country__name">${data.name}</h3>
+//           <h4 class="country__region">${data.region}</h4>
+//           <p class="country__row"><span>👫</span>${(
+//             +data.population / 1000000
+//           ).toFixed(1)} people</p>
+//           <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//           <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//         </div>
+//   </article>
+//     `;
 
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-  });
+//     countriesContainer.insertAdjacentHTML("beforeend", html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+const renderCountry = function (data) {
+  const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+</article>
+  `;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
 };
-getCountryData("Croatia");
-getCountryData("itali");
-getCountryData("slovenija");
-getCountryData("united states");
+
+// const getCountryAndNeighbour = function (country) {
+//   const xmlRequest = new XMLHttpRequest();
+//   xmlRequest.open("GET", `https://restcountries.com/v2/name/${country}`);
+//   xmlRequest.send();
+//   xmlRequest.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     renderCountry(data);
+//   });
+// };
+// getCountryAndNeighbour("croatia");
+
+const fetchRequest = fetch(`https://restcountries.com/v2/name/croatia`);
+console.log(fetchRequest);
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => renderCountry(data[0]));
+};
+getCountryData("croatia");
+getCountryData("usa");
+getCountryData("Itali");
